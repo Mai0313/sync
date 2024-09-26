8827b96d9f543f845082a84312a382bcad634268
Wei <21039366+Mai0313@users.noreply.github.com>
2024-09-26 12:09:58 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -6,8 +6,6 @@ TARGET_IP=${TARGET_IP:-172.21.148.163}  # 同步到哪台機器
 DELETE_NOT_EXIST=${DELETE_NOT_EXIST:-false}  # 默認不刪除目標目錄中來源目錄沒有的檔案
 EXCLUDE_FOLDERS=()  # 在這裡添加你要排除的資料夾
 
-echo "Syncing files to $USERNAME's home directory on $TARGET_IP"
-
 # 構建 --exclude 選項
 EXCLUDE_OPTIONS=""
 for folder in "${EXCLUDE_FOLDERS[@]}"; do
@@ -20,9 +18,6 @@ if [ "$DELETE_NOT_EXIST" = true ]; then
     DELETE_OPTION="--delete"
 fi
 
-echo "Excluding folders: ${EXCLUDE_FOLDERS[@]}"
-echo "Delete not exist: $DELETE_NOT_EXIST"
-
 # 構建來源目錄
 SOURCE_DIR=""
 if [ "$SOURCE_IP" = "$(hostname -I | awk '{print $1}')" ]; then
@@ -31,6 +26,9 @@ else
     SOURCE_DIR="$USERNAME@$SOURCE_IP:~/"
 fi
 
+echo "Syncing files to $USERNAME's home directory on $TARGET_IP"
+echo "Excluding folders: ${EXCLUDE_FOLDERS[@]}"
+echo "Delete not exist: $DELETE_NOT_EXIST"
 echo "Source directory: $SOURCE_DIR"
 
-eval rsync -avz --progress $EXCLUDE_OPTIONS $DELETE_OPTION $SOURCE_DIR $USERNAME@$TARGET_IP:~/
+eval rsync -avz $EXCLUDE_OPTIONS $DELETE_OPTION $SOURCE_DIR $USERNAME@$TARGET_IP:~/
