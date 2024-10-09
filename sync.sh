7ea8b0625f483b9899d0583b80c507d6d13601f5
Wei <21039366+Mai0313@users.noreply.github.com>
2024-10-09 12:56:16 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,7 +4,7 @@ USERNAME=$(whoami)  # 取得用戶名稱
 SOURCE_IP=${SOURCE_IP:-$(hostname -I | awk '{print $1}')}  # 來自哪台機器，如果沒有提供，則使用當前機器的 IP 地址
 TARGET_IP=${TARGET_IP:-172.21.148.163}  # 同步到哪台機器
 DELETE_NOT_EXIST=${DELETE_NOT_EXIST:-false}  # 默認不刪除目標目錄中來源目錄沒有的檔案
-EXCLUDE_FOLDERS=(**/.venv)  # 在這裡添加你要排除的資料夾
+EXCLUDE_FOLDERS=(**/.venv **/.cache)  # 在這裡添加你要排除的資料夾
 
 # 構建 --exclude 選項
 EXCLUDE_OPTIONS=""
